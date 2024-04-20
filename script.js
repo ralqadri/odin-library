a8006e3a5c2858e11f6a30d7b7eb848d47c05a0e
rizki a <rzkalqdr@gmail.com>
2024-04-20 17:03:21 +0700

    finish: working delete button for each card

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const myLibrary = [];
 
-const cardContainer = document.querySelector(".cards");
-// console.log(cardContainer);
+class Book {
+	constructor(
+		name = "Unknown",
+		author = "Unknown Author",
+		pages = 0,
+		read = false
+	) {
+		this.name = name;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+	}
+}
 
-function Book(name, author, pages) {
-	this.name = name ? name : "Unknown";
-	this.author = author ? author : "Unknown Author";
-	this.pages = pages ? pages : 0;
+const cardContainer = document.querySelector(".cards");
+
+// BOOK RELATED FUNCTION(S)
+
+function printAllBooks() {
+	myLibrary.forEach(function (book) {
+		console.log(book);
+	});
+}
+
+function searchBook(search) {
+	return myLibrary.find(({ name }) => name === search);
+}
+
+function searchIndexBook(search) {
+	return myLibrary.findIndex(({ name }) => name === search);
 }
 
 function addBookToLibrary(name, author, pages) {
@@ -15,46 +38,75 @@ function addBookToLibrary(name, author, pages) {
 	addBookToPage(newBook);
 }
 
-function printAllBooks() {
-	myLibrary.forEach(function (book) {
-		console.log(book);
-	});
-}
-
-function showAllBooks() {
-	myLibrary.forEach(function (book) {
-		addBookToPage(book);
-	});
-}
-
 function addBookToPage(book) {
 	const card = document.createElement("div");
 	const cardTitle = document.createElement("div");
 	const cardAuthor = document.createElement("div");
 	const cardPages = document.createElement("div");
+	const cardDelete = document.createElement("button");
 
 	card.classList.add("card");
 	cardTitle.classList.add("card-title");
 	cardAuthor.classList.add("card-author");
 	cardPages.classList.add("card-pages");
+	cardDelete.classList.add("card-delete");
 
 	cardTitle.textContent = book.name;
 	cardAuthor.textContent = book.author;
 	cardPages.textContent = book.pages;
+	cardDelete.textContent = "Delete";
+	cardDelete.onclick = removeBookCard;
 
 	card.appendChild(cardTitle);
 	card.appendChild(cardAuthor);
 	card.appendChild(cardPages);
+	card.appendChild(cardDelete);
+
+	// TODO: Add `value` reset here later
 
 	cardContainer.appendChild(card);
 }
 
-addBookToLibrary("Normal People", "Sally Rooney", 293);
-addBookToLibrary("Play It As It Lays", "Joan Didion", 192);
-addBookToLibrary("A Single Man", "Christopher Isherwood", 86);
-addBookToLibrary("Conversations on Love", "Natasha Lunn", 321);
-addBookToLibrary("Hall Of Small Mammals", "Thomas Pierce", 461);
-addBookToLibrary("A Little Life", "Hanya Yanagihara", 802);
+function resetCardContainer(cardContainer) {
+	cardContainer.innerHTML = "";
+}
+
+function updateCardContainer(cardContainer) {
+	resetCardContainer(cardContainer);
+	myLibrary.forEach(function (book) {
+		addBookToPage(book);
+	});
+}
+
+// TODO: Clean up console.logs later
+function removeBook(search) {
+	const index = searchIndexBook(search);
+	if (index !== -1) {
+		myLibrary.splice(index, 1);
+		console.log(`REMOVED: ${search}`);
+		updateCardContainer(cardContainer);
+	} else {
+		console.log(`NOT FOUND: ${search}`);
+	}
+}
+
+// A part of this code is inspired by Michal Osman
+function removeBookCard(e) {
+	const targetedCard = e.target.parentNode.firstChild.innerHTML;
+	console.log(`Targeted card is: ${targetedCard}`);
+	removeBook(targetedCard);
+}
+
+// DUMMY DATA (ADDING FILLER BOOKS TO LIBRARY)
+
+addBookToLibrary("Normal People", "Sally Rooney", 293, false);
+addBookToLibrary("Play It As It Lays", "Joan Didion", 192, false);
+addBookToLibrary("A Single Man", "Christopher Isherwood", 86, false);
+addBookToLibrary("Conversations on Love", "Natasha Lunn", 321, false);
+addBookToLibrary("Hall Of Small Mammals", "Thomas Pierce", 461, false);
+addBookToLibrary("A Little Life", "Hanya Yanagihara", 802, false);
+
+// DIALOG & MODALS (ADD BOOK POP-UP)
 
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector(".show-button");
@@ -69,3 +121,18 @@ addBookButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
 	dialog.close();
 });
+
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", submitClick, false);
+
+function submitClick(event) {
+	console.log("submit button clicked");
+	const titleInput = document.querySelector("#book-title").value;
+	const authorInput = document.querySelector("#book-author").value;
+	const pagesInput = document.querySelector("#book-pages").value;
+
+	addBookToLibrary(titleInput, authorInput, pagesInput);
+
+	event.preventDefault();
+	dialog.close();
+}
